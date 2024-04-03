31c0a0dcaa4b556570abf8e7588b6e40b5019463
Ziyodulla <admin@jbnuu.uz>
2024-04-03 12:10:48 +0500

    LastChanges

--- js/api.js
+++ js/api.js
@@ -1,11 +1,6 @@
-async function getProducts(){
-    const token = localStorage.getItem('token');
-    const resp = await fetch('https://api.escuelajs.co/api/v1/products',{
-        method: 'GET',
-        headers: {
-            "Authorization": `Bearer ${token}`
-        },
-    });
+async function getProducts(url){
+    //const token = localStorage.getItem('token');
+    const resp = await fetch(url);
     return await resp.json();
 }
 
--- js/index.js
+++ js/index.js
@@ -3,9 +3,10 @@ import * as helpers from './helpers.js';
 
 const navBar = document.getElementById('navbar');
 
-const token = await api.getToken('john@mail.com','changeme');
+const LIMIT =  12;
+//const token = await api.getToken('john@mail.com','changeme');
 
-helpers.saveToLocalStorage('token',token.access_token);
+//helpers.saveToLocalStorage('token',token.access_token);
 
 // console.log(token.access_token);
 
@@ -39,30 +40,66 @@ navBar.append(createLoginForm());
 
 const productsRow = document.querySelector('.main-good-list');
 
-const products = await api.getProducts();
+const getProductsFunc = async (page) => {
+    const products = await api.getProducts(`https://dummyjson.com/products?limit=${LIMIT}&skip=${(page - 1) * LIMIT}`);
 
-console.log(products);
+    return await products
+}
 
-products.forEach((el,indx) => {
-    const oneProduct = `
-    <div class="col-sm-6 col-md-4">
-        <div class="thumbnail">
-        <img class="resp" src="${el.images[0]}" alt="${el.title}">
-        <div class="caption">
-            <h3 class="line-clamp">${el.title}</h3>
-            <p class="line-clamp">${el.description}</p>
-            <p>
-                <a href="#" class="btn btn-primary" role="button">${el.price} $</a> 
-                <a href="#" class="btn btn-success" role="button">More information</a>
-            </p>
-        </div>
-        </div>
-    </div>`;
+function printPagination(){
+    const  total = 100 / LIMIT;
+    console.log(Math.ceil(total));
+    
+    const paginationWrap = document.querySelector('.pagination');
+    for(let i = 1; Math.ceil(total) >= i; i ++){
+        
+        const paginationList = helpers.createEl('li', ['list']);
+        const paginationLink = helpers.createEl('a',['link']);
+        paginationLink.addEventListener('click',async (event) => {
+            const page = event.target.innerHTML;
+            const products = await getProductsFunc(page);
+            //console.log(products);
+            productsRow.innerHTML = '';
+            printProducts(products);
+        });
+        paginationList.append(paginationLink);
+        paginationLink.textContent += i;
+        paginationWrap.append(paginationList);
+    }
+    
 
-    const moreBtn = document.querySelector('.btn-success');
-    moreBtn.addEventListener('click',(eve)=>{
-        console.log(eve);
+    return paginationWrap;
+}
+
+
+const products = await getProductsFunc(0);
+
+function printProducts(products){
+    products['products'].forEach((el,indx) => {
+        const oneProduct = `
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+            <img class="resp" src="${el.images[0]}" alt="${el.title}">
+            <div class="caption">
+                <h3 class="line-clamp">${el.title}</h3>
+                <p class="line-clamp">${el.description}</p>
+                <p>
+                    <a href="#" class="btn btn-primary" role="button">${el.price} $</a> 
+                    <a href="#" class="btn btn-success" role="button">More information</a>
+                </p>
+            </div>
+            </div>
+        </div>`;
+    
+        // const moreBtn = document.querySelector('.btn-success');
+        // moreBtn.addEventListener('click',(eve)=>{
+        //     console.log(eve);
+        // });
+    
+        productsRow.innerHTML += oneProduct;
     });
+}
 
-    productsRow.innerHTML += oneProduct;
-});
+printProducts(products);
+
+printPagination();
